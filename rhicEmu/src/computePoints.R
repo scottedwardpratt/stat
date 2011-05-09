9d86ef8dafed7f2d23c4c3714306836b397bb314
ccs <cec24@phy.duke.edu>
2011-05-08 22:52:04 -0400

    prior errors plus error from emulator now combine in quadrature
    errors combine in quad, makes them better :) Also moved from
    reconCurveAtPoint to reconCurveAtList which makes a huge difference in
    running speed.

--- rhicEmu/src/computePoints.R
+++ rhicEmu/src/computePoints.R
@@ -1,5 +1,7 @@
 ## computePoints:
 ## ccs, cec24@phy.duke.edu 2011-March/April (msu)
+## 1) updated to use reconCurveAtList which is much faster
+## than reconCurveAtPoint.
 ##
 ## produces mean and variances for a set of points in the parameter space
 ## 
@@ -14,11 +16,14 @@
 ##
 
 ## see if we should rescale
-cargs <- Sys.getenv(c('rescale', 'rescaleErr', 'inpath'))
+cargs <- Sys.getenv(c('rescale', 'rescaleErr', 'inpath', 'EMU_DIRECTORY'))
 rescale <- as.numeric(cargs[1])
 rescaleErr <- as.numeric(cargs[2])
 inpath <- cargs[3]
 directory <- cargs[4]
+
+cat("inpath: ", inpath, "\n")
+cat("directory: ", inpath, "\n")
 
 source(paste(directory, "/gen-samp.R", sep = "")) # load the sample set
 source(paste(directory, "/emu-pca.R", sep = "")) # load the pca fns
@@ -28,7 +33,7 @@ source(paste(directory, "/emu-pca.R", sep = "")) # load the pca fns
 dyn.load("~/local/lib/libRBIND.dylib") #
 # moving these files into the project root (or installing them) is probably best
 source("~/local/include/libRbind/EmuRbind.R")
-source("~/local/include/libRbind/multivar.R")
+#source("~/local/include/libRbind/multivar.R")
 
 
 ## ccs: for debugging
@@ -57,7 +62,7 @@ thetas.est <- read.table(buffer)
 
 ## now we want to read the stdin
 #points <- read.table("/dev/stdin") # sadly this doesn't work
-points <- read.table("temp")
+points <- as.matrix(read.table("temp"))
 npoints <- dim(points)[1]
 numpoints <-dim(points)[2]
 
@@ -71,33 +76,51 @@ rvalues <- vector("list", npoints)
 rvaluesRescaled <- vector("list", npoints)
 
 # compute the curves in t-space at each point
+# this version takes < 1 second for 330 points,
+# much more efficient.
+rvalues <- reconCurveAtList(points, thetas.est, pca.decomp)
+
+# REFERENCE ONLY
+# this was the old method, a lot of setup and teardown had to be done
+# for each point, making it very slow
+## print(points)
+## for(i in 1:336){ # 17 seconds
+## this isn't actually practical, it's just to test how much slower a single point would be
+##   rvaluesRescaled <- reconCurveAtPoint(points[1,], thetas.est, pca.decomp)
+## }
+
 for(i in 1:npoints){
-  rvalues[[i]] <- reconCurveAtPoint(points[i,], thetas.est, pca.decomp)
   if(rescale==1){
   # undo the centering and scaling we did in gen.sample
   # with the stored data from sample
-    rvaluesRescaled[[i]]$mean <- rvalues[[i]]$mean * sample$sds + sample$means
-    rvaluesRescaled[[i]]$var <- rvalues[[i]]$var * sample$sds
+    rvaluesRescaled[[i]]$mean <- rvalues$mean[,i] * sample$sds + sample$means
+    rvaluesRescaled[[i]]$var <- rvalues$var[,i] * sample$sds
     # copy in the unmodified things
-    rvaluesRescaled[[i]]$xpos <- rvalues[[i]]$xpos
-    rvaluesRescaled[[i]]$tvalues <- rvalues[[i]]$tvalues
+    rvaluesRescaled[[i]]$xpos <- rvalues$xpos[i,]
+    rvaluesRescaled[[i]]$tvalues <- rvalues$tvalues
   } else if(rescaleErr ==1){
     cat("#using rescale error!\n")
+    print(rvalues$mean[,i])
+    print(rvalues$xpos[i,])
     ## escale with predetermined errors instead of using the sampleerrors
-    rvaluesRescaled[[i]]$mean <- (rvalues[[i]]$mean + sample$means) * sample$errs[i,]
+    rvaluesRescaled[[i]]$mean <- (rvalues$mean[,i] + sample$means) * sample$errs[i,]
     # this is not coming out to the right scale at all
     # \todo fix the rescaled variance
-    rvaluesRescaled[[i]]$var <- rvalues[[i]]$var * sample$errs[i,]
+    # errors combine in quadrature, dumbass
+    rvaluesRescaled[[i]]$var <- sqrt((rvalues$var[,i]*rvalues$var[,i]) + (sample$errs[i,]*sample$errs[i,]))
 
     # copy in the unmodified things
-    rvaluesRescaled[[i]]$xpos <- rvalues[[i]]$xpos
-    rvaluesRescaled[[i]]$tvalues <- rvalues[[i]]$tvalues
+    rvaluesRescaled[[i]]$xpos <- rvalues$xpos[i,]
+    rvaluesRescaled[[i]]$tvalues <- rvalues$tvalues
   }
 }
 
 # now we want to scale the curves back to the values we had before centering
 for(i in 1:npoints){
+  # this just to make the output easier to look at
+  cat("\n")
   write.table(rvaluesRescaled[[i]]$mean, stdout(), row.names=FALSE, col.names=FALSE)
+  cat("\n")
   write.table(rvaluesRescaled[[i]]$var, stdout(), row.names=FALSE, col.names=FALSE)
 	if(any(is.na(rvaluesRescaled[[i]]$mean)==TRUE)||any(is.na(rvaluesRescaled[[i]]$var)==TRUE) ){
 		stop("NA values in emulator output.")
