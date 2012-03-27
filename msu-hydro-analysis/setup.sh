606728f9c4eeaa942733d260bfa55880cf437e68
John Novak <novakj@nscl.msu.edu>
2012-03-27 12:57:14 -0400

    Modified scripts for creating trained emulator with multiple centrality classes

--- msu-hydro-analysis/setup.sh
+++ msu-hydro-analysis/setup.sh
@@ -43,21 +43,17 @@ echo "# creating default combined analysis files"
 ## we'll create files for cents 1 2 3 for hbt,spec and spec and flow
 
 ## if you want to add another output type you should add its name here
-##outputNames=(spectra-combined hbt-combined spec-and-flow-combined custom)
-outputNames=(custom)
+outputNames=(spectra-combined hbt-combined spec-and-flow-combined custom)
+## outputNames=(custom)
 ## here you can add another centrality class, we currently have data for 30to40 and 40to50
-centNames=(0to5 10to20 20to30)
-##centNames=(0to5)
+centNames=(cent0to5 allb cent20to30)
 ## and you should add the name of the specification file here
 #specFiles=(spectra-only-spec.dat hbt-only-spec.dat star-spectra-and-flow.dat)
 
 countSpec=1
 for i in $outputNames; do
-		countCent=1
 		for j in $centNames; do
-#				./data-prep/generate-analysis-files.rb $1/analysis $countCent $1/$i-$j.dat $1/$i-$j-errors.dat <./data-prep/$specFiles[$countSpec] $countSpec $countCent
-				./data-prep/generate-analysis-files.rb $1/analysis $countCent $1/$i-$j.dat $1/$i-$j-errors.dat <./data-prep/$i.dat $countSpec $countCent
-				countCent=$((countCent+1))
+				./data-prep/generate-analysis-files.rb $1/analysis $j $1/$i-$j.dat $1/$i-$j-errors.dat <./data-prep/$i.dat $countSpec $countCent
 		done
 		countSpec=$((countSpec+1))
 done
