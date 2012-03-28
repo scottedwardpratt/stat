5efc58fe03d56fcbd938055fdd82b4347a6e89c1
John Novak <novakj@nscl.msu.edu>
2012-03-28 14:08:24 -0400

    changed some of the scripts setting up stuff for the emulator

--- msu-hydro-analysis/setup.sh
+++ msu-hydro-analysis/setup.sh
@@ -43,10 +43,10 @@ echo "# creating default combined analysis files"
 ## we'll create files for cents 1 2 3 for hbt,spec and spec and flow
 
 ## if you want to add another output type you should add its name here
-outputNames=(spectra-combined hbt-combined spec-and-flow-combined custom)
-## outputNames=(custom)
+outputNames=(standard)
 ## here you can add another centrality class, we currently have data for 30to40 and 40to50
-centNames=(cent0to5 allb cent20to30)
+centNames=(allb)
+## centNames=(cent0to5 allb cent20to30)
 ## and you should add the name of the specification file here
 #specFiles=(spectra-only-spec.dat hbt-only-spec.dat star-spectra-and-flow.dat)
 
