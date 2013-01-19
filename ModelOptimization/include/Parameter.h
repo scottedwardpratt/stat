eb07910bb496584059a232a6d830fd5504747d27
Cory Quammen <cquammen@cs.unc.edu>
2013-01-18 21:46:53 -0500

    Remove unused CParameterInfor and CParameterList classes

--- ModelOptimization/include/Parameter.h
+++ ModelOptimization/include/Parameter.h
@@ -21,18 +21,6 @@ See copyright.txt for more information.
 namespace madai {
 	using namespace std;
 	
-	class CParameterInfo {
-	public:
-			//ParameterInfo() {};
-			//virtual ~ParameterInfo() {};
-		string Name;
-		unsigned int index;
-		double MinimumPossibleValue;
-		double MaximumPossibleValue;
-		double BestValue;
-		
-	}; // end class ParameterInfo
-
 	class Parameter {
 	public:
 		Parameter(std::string nm, 
@@ -47,17 +35,6 @@ namespace madai {
 		double      m_MaximumPossibleValue;
 	}; // end class Parameter
 	
-	class CParameterList{
-	public:
-		vector<CParameterInfo> *parinfo;
-		CParameterInfo *GetParInfo(unsigned int parindex);
-		CParameterInfo *GetParInfo(string parname);
-		void AddParameter(string parname,double maxvalue,double minvalue);
-		void DeleteParameter(string parname);
-		void DeleteParameter(int parindex);
-		int Length;
-	};
-
 } // end namespace madai
 
 #endif // __Parameter_h_
