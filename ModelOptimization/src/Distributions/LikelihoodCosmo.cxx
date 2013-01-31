ca42bb07b7b54de08ffafcfe6157a65ce2376156
Cory Quammen <cquammen@cs.unc.edu>
2013-01-31 15:44:33 -0500

    Style improvements to distribution classes

--- ModelOptimization/src/Distributions/LikelihoodCosmo.cxx
+++ ModelOptimization/src/Distributions/LikelihoodCosmo.cxx
@@ -1,108 +1,126 @@
 #include "Distribution.h"
 #include <time.h>
 
-madai::LikelihoodDistribution_Cosmo::LikelihoodDistribution_Cosmo(madai::Model *in_Model){
-  m_Model = in_Model;
-  m_SepMap = parameter::getB(m_Model->m_ParameterMap, "LIKELIHOOD_PARAMETER_MAP", false);
+namespace madai {
 
-  if(m_SepMap){
-  std::string parmapfile = m_Model->m_DirectoryName + "defaultpars/likelihood.param";
-  m_ParameterMap = new parameterMap;
-  parameter::ReadParsFromFile(*m_ParameterMap, parmapfile);
-  }else{
-  m_ParameterMap = &(m_Model->m_ParameterMap);
+LikelihoodDistribution_Cosmo
+::LikelihoodDistribution_Cosmo( Model *in_Model )
+{
+  m_Model = in_Model;
+  m_SepMap = parameter::getB( m_Model->m_ParameterMap, "LIKELIHOOD_PARAMETER_MAP", false );
+
+  if ( m_SepMap ) {
+    std::string parmapfile = m_Model->m_DirectoryName + "defaultpars/likelihood.param";
+    m_ParameterMap = new parameterMap;
+    parameter::ReadParsFromFile( *m_ParameterMap, parmapfile );
+  } else {
+    m_ParameterMap = &(m_Model->m_ParameterMap);
   }
 
   // cout << "Like param map made." << endl;
 
-  m_Timing = parameter::getB(*m_ParameterMap, "TIMING", false) || parameter::getB(*m_ParameterMap, "TIME_LIKELIHOOD", false);
-  m_Verbose = parameter::getB(*m_ParameterMap, "VERBOSE", false) || parameter::getB(*m_ParameterMap, "VERBOSE_LIKELIHOOD", false);
+  m_Timing = parameter::getB( *m_ParameterMap, "TIMING", false) || parameter::getB(*m_ParameterMap, "TIME_LIKELIHOOD", false );
+  m_Verbose = parameter::getB( *m_ParameterMap, "VERBOSE", false) || parameter::getB(*m_ParameterMap, "VERBOSE_LIKELIHOOD", false );
 
   m_Data = GetData();
-  m_intData.resize(m_Data.size());
-  for(int i = 1; i < m_intData.size(); i++){
-  m_intData[i]=gsl_ran_poisson(m_RandNumGen,m_Data[i]);
+  m_intData.resize( m_Data.size() );
+  for ( int i = 1; i < m_intData.size(); i++ ) {
+    m_intData[i] = gsl_ran_poisson( m_RandNumGen, m_Data[i] );
   }
 }
 
-madai::LikelihoodDistribution_Cosmo::~LikelihoodDistribution_Cosmo(){
+
+LikelihoodDistribution_Cosmo
+::~LikelihoodDistribution_Cosmo()
+{
   //delete emulator;
 }
 
-double madai::LikelihoodDistribution_Cosmo::Evaluate(std::vector<double> ModelMeans,
-                                                     std::vector<double> ModelErrors){
+
+double
+LikelihoodDistribution_Cosmo
+::Evaluate( std::vector<double> ModelMeans,
+            std::vector<double> ModelErrors )
+{
   clock_t begintime;
   double likelihood = 0.0,dll;
 
-  if(m_Timing){
-  begintime = clock();
+  if ( m_Timing ) {
+    begintime = clock();
   }
 
-  for(int i = 1; i < ModelMeans.size(); i++){
-  dll=log(gsl_ran_poisson_pdf(m_intData[i],ModelMeans[i]));
-  //printf("ModelMeans[%d]=%g, intDATA[%d]=%d, Dloglikelihood=%g\n",i,ModelMeans[i],i,intDATA[i],dll);
-  //likelihood += log(gsl_ran_poisson_pdf(static_cast<unsigned int>(ModelMeans[i] + 0.5), DATA[i]));
-  likelihood += dll;
+  for ( int i = 1; i < ModelMeans.size(); i++ ) {
+    dll = log( gsl_ran_poisson_pdf( m_intData[i],ModelMeans[i] ) );
+    //printf("ModelMeans[%d]=%g, intDATA[%d]=%d, Dloglikelihood=%g\n",i,ModelMeans[i],i,intDATA[i],dll);
+    //likelihood += log(gsl_ran_poisson_pdf(static_cast<unsigned int>(ModelMeans[i] + 0.5), DATA[i]));
+    likelihood += dll;
   }
-  printf("XXXXX LogLikelihood=%g XXXXXX\b",likelihood);
-  if(likelihood>-0.0001){
-  for(int i = 1; i < ModelMeans.size(); i++){
-  dll=log(gsl_ran_poisson_pdf(m_intData[i],ModelMeans[i]));
-  printf("ModelMeans[%d]=%g, intDATA[%d]=%d, Dloglikelihood=%g\n",i,ModelMeans[i],i,m_intData[i],dll);
-  //likelihood += log(gsl_ran_poisson_pdf(static_cast<unsigned int>(ModelMeans[i] + 0.5), DATA[i]));
-  }
-  exit(1);
-  }
-
-  if(!(m_Model->m_LogLike)){
-  likelihood = exp(likelihood);
+  printf( "XXXXX LogLikelihood=%g XXXXXX\b", likelihood );
+  if ( likelihood > -0.0001 ) {
+    for ( int i = 1; i < ModelMeans.size(); i++ ) {
+      dll = log( gsl_ran_poisson_pdf( m_intData[i], ModelMeans[i] ) );
+      printf( "ModelMeans[%d]=%g, intDATA[%d]=%d, Dloglikelihood=%g\n", i, ModelMeans[i], i, m_intData[i], dll );
+      //likelihood += log(gsl_ran_poisson_pdf(static_cast<unsigned int>(ModelMeans[i] + 0.5), DATA[i]));
+    }
+    exit( 1 );
   }
 
-  if(m_Timing){
-  std::cout << "Likelihood evaluation took " << (clock()-begintime)*1000/CLOCKS_PER_SEC << " ms." << std::endl;
+  if ( !(m_Model->m_LogLike) ) {
+    likelihood = exp( likelihood );
+  }
+
+  if ( m_Timing ) {
+    std::cout << "Likelihood evaluation took " << (clock()-begintime)*1000/CLOCKS_PER_SEC << " ms." << std::endl;
   }
 
   return likelihood;
 }
 
-std::vector<double> madai::LikelihoodDistribution_Cosmo::GetData(){
-  std::vector<double> datameans;
+
+std::vector< double >
+LikelihoodDistribution_Cosmo
+::GetData()
+{
+  std::vector< double > datameans;
   std::stringstream ss;
   parameterMap actualparmap;
   std::ifstream inputfile;
 
   std::string actual_filename = m_Model->m_DirectoryName + "/defaultpars/actual.param";
-  parameter::ReadParsFromFile(actualparmap, actual_filename);
+  parameter::ReadParsFromFile( actualparmap, actual_filename );
 
-  std::vector<std::string> temp_names = parameter::getVS(actualparmap, "NAMES", "");
-  std::vector<double> temp_values = parameter::getV(actualparmap, "VALUES", "");
+  std::vector< std::string > temp_names = parameter::getVS( actualparmap, "NAMES", "" );
+  std::vector< double > temp_values = parameter::getV( actualparmap, "VALUES", "" );
 
-  std::vector<double> ActualParamValues;
-  std::vector<std::string> ActualParamNames;
+  std::vector< double > ActualParamValues;
+  std::vector< std::string > ActualParamNames;
   ActualParamValues = temp_values;
   ActualParamNames = temp_names;
 
   ss << "cosmosurvey";
-  for(int i = 0; i < temp_names.size(); i++){
-  ss << " -" << temp_names[i] << " " << temp_values[i];
+  for ( int i = 0; i < temp_names.size(); i++ ) {
+    ss << " -" << temp_names[i] << " " << temp_values[i];
   }
   ss << " -nz 10 -nf 10 -ob .0406 > data_output.dat" << endl;
 
   std::cout << ss.str() << std::endl;
   std::cout << "Waiting on cosmosurvey...";
   std::cout.flush();
-  int result = system((ss.str()).c_str());
+  int result = system( (ss.str()).c_str() );
   std::cout << "Done." << std::endl;
 
-  inputfile.open("data_output.dat");
+  inputfile.open( "data_output.dat" );
 
   int counter = 0;
-  while(!inputfile.eof()){
-  counter++;
-  double temp;
-  inputfile >> temp;
-  std::cout << "Data point " << counter << ": " << temp << std::endl;
-  datameans.push_back(temp);
+  while ( !inputfile.eof() ) {
+    counter++;
+    double temp;
+    inputfile >> temp;
+    std::cout << "Data point " << counter << ": " << temp << std::endl;
+    datameans.push_back( temp );
   }
+
   return datameans;
 }
+
+} // end namespace madai
--- ModelOptimization/src/Distributions/PriorTest.cxx
+++ ModelOptimization/src/Distributions/PriorTest.cxx
@@ -1,44 +1,54 @@
 #include "Model.h"
 #include "Distribution.h"
 
-madai::PriorDistribution_Test::PriorDistribution_Test(madai::Model * in_Model){
-  m_Model = in_Model;
-  m_SepMap = parameter::getB(m_Model->m_ParameterMap, "PRIOR_PARAMETER_MAP", false);
+namespace madai {
 
-  if(m_SepMap){
-  std::string parmapfile = m_Model->m_DirectoryName + "/parameters/prior.param";
-  m_ParameterMap = new parameterMap;
-  parameter::ReadParsFromFile(*m_ParameterMap, parmapfile);
-  //parameter::ReadParsFromFile(parmap, parameter_file_name);
-  }else{
-  m_ParameterMap = &(m_Model->m_ParameterMap);
+PriorDistribution_Test
+::PriorDistribution_Test( Model * in_Model )
+{
+  m_Model = in_Model;
+  m_SepMap = parameter::getB( m_Model->m_ParameterMap, "PRIOR_PARAMETER_MAP", false );
+
+  if ( m_SepMap ) {
+    std::string parmapfile = m_Model->m_DirectoryName + "/parameters/prior.param";
+    m_ParameterMap = new parameterMap;
+    parameter::ReadParsFromFile( *m_ParameterMap, parmapfile );
+    //parameter::ReadParsFromFile(parmap, parameter_file_name);
+  } else {
+    m_ParameterMap = &(m_Model->m_ParameterMap);
   }
 }
 
-double madai::PriorDistribution_Test::Evaluate(std::vector<double> Theta){
-  double mean = parameter::getD(*m_ParameterMap, "PRIOR_MEAN", -3.7372);
-  double sigma = parameter::getD(*m_ParameterMap, "PRIOR_SIGMA", 1.6845);
+
+double
+PriorDistribution_Test
+::Evaluate( std::vector< double > Theta ) {
+  double mean  = parameter::getD( *m_ParameterMap, "PRIOR_MEAN", -3.7372 );
+  double sigma = parameter::getD( *m_ParameterMap, "PRIOR_SIGMA", 1.6845 );
   double temp = 0.0;
   bool Found = false;
 
-  std::vector<madai::Parameter> const * parameters = &(m_Model->GetParameters());
-  for(int i = 0; i< parameters->size(); i++){
-  if((*parameters)[i].m_Name.compare(0,1,"SIGMA")==0){
-  if(!Found){
-  temp = Normal(log(Theta[i]), mean, sigma);
-  Found = true;
-  }else{
-  std::cerr << "In RHIC_PCA_PRIOR::Evaluate; Duplicate parameter names found." << std::endl;
-  exit(1);
+  std::vector< Parameter > const * parameters = &(m_Model->GetParameters());
+  for ( int i = 0; i < parameters->size(); i++ ) {
+    if ( (*parameters)[i].m_Name.compare( 0, 1, "SIGMA" ) == 0 ) {
+      if ( !Found ) {
+        temp = Normal( log( Theta[i] ), mean, sigma );
+        Found = true;
+      } else {
+        std::cerr << "In RHIC_PCA_PRIOR::Evaluate; Duplicate parameter names found." << std::endl;
+        exit( 1 );
+      }
+    }
   }
-  }
-  }
-  if(Found){
-  return temp;
+  if ( Found ) {
+    return temp;
   } else {
-  std::cerr << "SIGMA parameter not found!" << std::endl;
-  std::cerr << "Will return the prior as 1.0" << std::endl;
-  return 1.0;
+    std::cerr << "SIGMA parameter not found!" << std::endl;
+    std::cerr << "Will return the prior as 1.0" << std::endl;
+
+    return 1.0;
   }
   //return Normal(log(Theta.GetValue("SIGMA")), mean, sigma);
 }
+
+} // end namespace madai
