336d336044042a5638e19a117fe3869078875a70
Cory Quammen <cquammen@cs.unc.edu>
2013-01-31 14:26:10 -0500

    Added files in src/Distributions

--- ModelOptimization/maintenance/StyleCheck.sh
+++ ModelOptimization/maintenance/StyleCheck.sh
@@ -3,6 +3,6 @@
 # To use, run this from the top level of the ModelOptimization
 # library source directory.
 
-files=`ls -1 include/*.h src/*.cxx`
+files=`ls -1 include/*.h src/*.cxx src/Distributions/*.cxx`
 
 for file in $files; do KWStyle -v $file; done
