69e91dc509555f54dcb363851ae76216bea3c5c7
Cory Quammen <cquammen@cs.unc.edu>
2013-02-01 00:47:49 -0500

    Added license text to files

--- ModelOptimization/src/ExternalModel.cxx
+++ ModelOptimization/src/ExternalModel.cxx
@@ -1,13 +1,21 @@
-/*********************************************************************
-MADAI Model Statistical Tools
-Copyright 2011-2012, The University of North Carolina at Chapel Hill.
+/*=========================================================================
+ *
+ *  Copyright (c) 2010-2012 The University of North Carolina at Chapel Hill
+ *  All rights reserved.
+ *
+ *  Licensed under the MADAI Software License. You may obtain a copy of
+ *  this license at
+ *
+ *         https://madai-public.cs.unc.edu/software/license/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 
-This software was written in 2011-2012 by
-  Hal Canary <hal AT cs.unc.edu>
-while working for the MADAI project <http://madai.us/>.
-
-See copyright.txt for more information.
-*********************************************************************/
 #include <cassert>
 #include <cctype>
 #include <cstdio> // std::fgets, std::fscanf, et cetera.
--- ModelOptimization/src/Optimizer.h
+++ ModelOptimization/src/Optimizer.h
@@ -1,17 +1,21 @@
-/*********************************************************************
-MADAI Model Statistical Tools
-Copyright 2011-2012, The University of North Carolina at Chapel Hill.
+/*=========================================================================
+ *
+ *  Copyright (c) 2010-2012 The University of North Carolina at Chapel Hill
+ *  All rights reserved.
+ *
+ *  Licensed under the MADAI Software License. You may obtain a copy of
+ *  this license at
+ *
+ *         https://madai-public.cs.unc.edu/software/license/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 
-This software was written in 2011-2012 by
-  Cory Quammen <cquammen AT cs.unc.edu>
-  Russell Taylor <taylorr AT cs.unc.edu>
-  Scott Pratt <pratt AT nscl.msu.edu>
-  Kevin Novak <novakkev AT msu.edu>
-  Hal Canary <hal AT cs.unc.edu>
-while working for the MADAI project <http://madai.us/>.
-
-See copyright.txt for more information.
-*********************************************************************/
 #ifndef __Optimizer_h__
 #define __Optimizer_h__
 
