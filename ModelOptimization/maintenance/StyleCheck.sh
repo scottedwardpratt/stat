990f79fa543c5cd803086e92056c25e0cd977f83
Cory Quammen <cquammen@cs.unc.edu>
2013-02-05 17:28:58 -0500

    Fixed directories to process
    Added checking of headers, which weren't being checked before

--- ModelOptimization/maintenance/StyleCheck.sh
+++ ModelOptimization/maintenance/StyleCheck.sh
@@ -3,6 +3,6 @@
 # To use, run this from the top level of the ModelOptimization
 # library source directory.
 
-files=`ls -1 include/*.h src/*.cxx src/Distributions/*.cxx`
+files=`ls -1 src/*.h src/*.cxx`
 
 for file in $files; do KWStyle -v $file; done
